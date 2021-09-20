4c59ea1b4ab50e1786d3d4eb493f58b5f6413d85
Ivana <79462114+IvanaWinc@users.noreply.github.com>
2021-09-20 13:18:50 +0200

    Add files via upload

---
+++ array-loops-colors.js
@@ -0,0 +1,13 @@
+/*const colors = ["yellow", "blue", "red", "orange"];
+let i = 0;
+let text = "";
+
+while (i < colors.length) {
+    console.log(colors[i]);
+    i++;
+};*/
+
+
+const array1 = ['yellow', 'blue', 'red', 'orange'];
+
+array1.forEach(element => console.log(element));
---
+++ while-loop-colors.js
@@ -0,0 +1,8 @@
+const colors = ["yellow", "blue", "red", "orange"];
+let i = 0;
+let text = "";
+
+while (i < colors.length) {
+    console.log(colors[i]);
+    i++;
+};
